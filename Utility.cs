91921f440f7400f3197faa82ea21f02a45211230
sowmenappd <sowmen.appd@gmail.com>
2019-11-18 20:35:43 +0600

    Coord struct renamed to Coord2D

--- Utility.cs
+++ Utility.cs
@@ -5,9 +5,9 @@ using UnityEngine;
 
 namespace SRLibs {
 
-  public struct Coord {
+  public struct Coord2D {
     public int x, y;
-    public Coord (int x, int y) {
+    public Coord2D (int x, int y) {
       this.x = x;
       this.y = y;
     }
@@ -23,13 +23,13 @@ namespace SRLibs {
     ///returns a list of tile coordinates available in the same region as the starting coordinate
     ///value 0 = empty, value 1 = obstacle 
     ///</summary>
-    public static List<Coord> FloodFill (int[ , ] map, FillMode fill = FillMode.eight, Coord center = new Coord ()) {
+    public static List<Coord2D> FloodFill (int[ , ] map, FillMode fill = FillMode.eight, Coord2D center = new Coord2D ()) {
       int width = map.GetLength (1);
       int height = map.GetLength (0);
 
-      List<Coord> regionTiles = new List<Coord>();
+      List<Coord2D> regionTiles = new List<Coord2D>();
 
-      List<Coord> checkList = new List<Coord> ();
+      List<Coord2D> checkList = new List<Coord2D> ();
       bool[, ] marked = new bool[height,width];
       for (int i = 0; i < height; i++) {
         for (int j = 0; j < width; j++) {
@@ -48,7 +48,7 @@ namespace SRLibs {
       checkList.Add (center);
 
       while (checkList.Count > 0) {
-        Coord tile = checkList[0];
+        Coord2D tile = checkList[0];
         checkList.RemoveAt (0);
         regionTiles.Add(tile);
 
@@ -70,7 +70,7 @@ namespace SRLibs {
               }
             }
             if (map[surroundTileY, surroundTileX] == 0 && !marked[surroundTileY, surroundTileX]) {
-              Coord temp = new Coord (surroundTileX, surroundTileY);
+              Coord2D temp = new Coord2D (surroundTileX, surroundTileY);
               checkList.Add (temp);
               marked[surroundTileY, surroundTileX] = true;
             }
